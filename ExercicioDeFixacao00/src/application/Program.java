9562deb32fcf4ddaa617e74d675c9caee3bec4e8
Osvaldo Lucunde <osvaldoluc7@gmail.com>
2026-08-20 20:53:50 +0100

    new commit

--- ExercicioDeFixacao00/src/application/Program.java
+++ ExercicioDeFixacao00/src/application/Program.java
@@ -1,4 +1,27 @@
 package application;
 
-public class Program {
+import entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program
+{
+    public static void main(String[] args)
+    {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rect = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+        rect.Width = sc.nextDouble();
+        rect.Height = sc.nextDouble();
+
+        System.out.printf("Area: %.2f\n", rect.Area());
+        System.out.printf("Perimeter: %.2f\n", rect.Perimeter());
+        System.out.printf("Diagonal: %.2f", rect.Diagonal());
+
+
+    }
 }
--- ExercicioDeFixacao00/src/entities/Rectangle.java
+++ ExercicioDeFixacao00/src/entities/Rectangle.java
@@ -1,4 +1,22 @@
 package entities;
 
-public class Retangle {
+public class Rectangle
+{
+    public double Width;
+    public double Height;
+
+    public double Area()
+    {
+        return Width * Height;
+    }
+
+    public double Perimeter()
+    {
+        return 2 * (Width + Height);
+    }
+
+    public double Diagonal()
+    {
+        return Math.sqrt(Math.pow(Width, 2) + Math.pow(Height, 2));
+    }
 }
