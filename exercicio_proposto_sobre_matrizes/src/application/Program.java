fbad1de5a2021d25be7b83fbb408c0c0f39f1493
Osvaldo Lucunde <osvaldoluc7@gmail.com>
2026-09-05 17:10:03 +0100

    Recebaaa, new commit

---
+++ exercicio_proposto_sobre_matrizes/src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] mat = new int[n][m];
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.printf("%d ", mat[i][j]);
+
+            }
+            System.out.println();
+        }
+
+        int k = sc.nextInt();
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if(mat[i][j] == k) {
+                    System.out.println("Position " + i + "," + j + ":");
+                    if (j > 0) {
+                        System.out.println("Left: " + mat[i][j-1]);
+                    }
+                    if (i > 0) {
+                        System.out.println("Up: " + mat[i-1][j]);
+                    }
+                    if (j < mat[i].length-1) {
+                        System.out.println("Left: " + mat[i][j+1]);
+                    }
+                    if (i < mat.length-1) {
+                        System.out.println("Down: " + mat[i+1][j] );
+                    }
+                }
+
+            }
+
+        }
+        sc.close();
+    }
+}
