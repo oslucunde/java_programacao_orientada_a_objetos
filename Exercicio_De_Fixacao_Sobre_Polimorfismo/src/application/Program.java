dd8fdb0d353b25d67f792047db38dadc2c9c35bd
Osvaldo Lucunde <osvaldoluc7@gmail.com>
2026-09-18 23:59:10 +0100

    hahahaha++

--- Exercicio_De_Fixacao_Sobre_Polimorfismo/src/application/Program.java
+++ Exercicio_De_Fixacao_Sobre_Polimorfismo/src/application/Program.java
@@ -1,4 +1,52 @@
 package application;
 
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.print("Enter the number of employees: ");
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            System.out.println("Employee #" + i + " data:");
+            System.out.print("Outsourced (y/n)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Hours: ");
+            int hours = sc.nextInt();
+            System.out.print("Value per hour: ");
+            double valueperhour = sc.nextDouble();
+            if(ch == 'y') {
+                System.out.print("Additional charge: ");
+                double additionalcharge = sc.nextDouble();
+                Employee emp = new OutsourcedEmployee(name, hours, valueperhour, additionalcharge);
+                list.add(emp);
+            }
+            else {
+                Employee emp = new Employee(name, hours, valueperhour);
+                list.add(emp);
+            }
+
+            System.out.println();
+            System.out.println("PAYMENTS: ");
+            for (Employee emp: list) {
+                System.out.println(emp.getName() + " - $" + String.format("%.2f", emp.payment()));
+            }
+        }
+        sc.close();
+    }
 }
